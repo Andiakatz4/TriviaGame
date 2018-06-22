62be2bfac4a372a19303f94f4e40cb9ad4686bd6
Andiakatz4 <andiakatz@gmail.com>
2018-06-21 23:27:32 -0700

    added html, css and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+
+    //set variables 
+    var limitedTimer = 60;
+
+    setInterval(function () {
+        limitedTimer--;
+        console.log(limitedTimer)
+        //when the limitedTimer 
+        // is 0 
+        // game over! can add and hide things
+    }, 1000);
+
+});
+//You'll create a trivia form with multiple choice or true/false options (your choice).
+
+//Question 1: What is the name of the largest ocean on earth?
+
+    //Answer Options to Question 1
+
+    //Correct answer to Question 1
+        //Pacific Ocean
+
+//Q1: What is the: <input type="radio" id="myRadio">
+
+
+
+//Question 2: Name the three time periods of the dinosaurs.
+
+    //Answer Options to Question 2
+
+    //Correct answer to Question 2
+        //Triassic, Jurassic and Cretaceous
+
+//Question 3: What is the diameter of Earth?
+
+    //Answer Options to Question 3
+
+    //Correct answer to Question 3
+        //7,918 miles (12,742 km).
+
+
+//Question 4: What is the Auora Borealis in the Southern Hemisphere called?
+
+    //Answer Options to Question 4
+
+    //Correct answer to Question 4
+        //Auora Australis or the "Southern Lights"
+
+
+//Question 5: The highest type of clouds are?
+
+    //Answer Options to Question 5
+
+    //Correct answer to Question 5
+        //Cirrus at 27,000 ft
+
+//Question 6: What is the name of the deepest location in the world’s oceans?
+
+    //Answer Options to Question 6
+
+    //Correct answer to Question 6
+        //Mariana Trench
+
+//Question 7: Your average tree supplies oxygen for how many people?
+
+    //Answer Options to Question 7
+
+    //Correct answer to Question 7
+        //4 people
+
+//Question 8: The mass of the earth is made up mostly of which two elements?
+
+    //Answer Options to Question 8
+
+    //Correct answer to Question 8
+        //Iron (32%) and oxygen (30%)
+
+//Question 9: How long can it take for a falling raindrop to travel from a rainforest’s thick canopy to the floor?
+
+    //Answer Options to Question 9
+
+    //Correct answer to Question 9
+        //10 minutes
+
+//Question 10: About how many active volcanoes are on Earth today?
+
+    //Answer Options to Question 10
+
+    //Correct answer to Question 10
+        //About 1,900
+
+//The player will have a limited amount of time to finish the quiz. 
+
+//The game ends when the time runs out. 
+
+//The page will reveal "All Done!" and the number of correct answers, incorrect answers and unanswered questions.
+
+//Don't let the player pick more than one answer per question.
+
+//Don't forget to include a countdown timer.
+
